017f5ee3c9f2206c9c989388d50ae840ceb506e3
Rufael Fekadu <rufaelfekadu48@gmail.com>
2021-04-21 15:08:55 +0300

    notification finished

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -8,59 +8,72 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ConsumptionRequestRepository;
+use App\Repository\ConsumptionDeliveryRepository;
 use App\Repository\SellsRepository;
 use App\Repository\StockRequestRepository;
+use App\Repository\StockRepository;
 
 class NotificationController extends AbstractController
 {
     /**
      * @Route("/notification", name="notification", methods={"POST", "GET"})
      */
-    public function index(ConsumptionRequestRepository $consumptionRequestRepository, Request $request, StockRequestRepository $stockRequestRepository, SellsRepository $sellsRepository): Response
+    public function index(ConsumptionRequestRepository $consumptionRequestRepository, Request $request, StockRequestRepository $stockRequestRepository, StockRepository $stockRepository, ConsumptionDeliveryRepository $consumptionDeliveryRepository, SellsRepository $sellsRepository): Response
     {
       
+
     //   $consumptionRequest =$consumptionRequestRepository->findBy('approvalStatus',null);
       $consumptionRequest = $consumptionRequestRepository->findBy(['approvalStatus' => 1]);
-      $totalConsumption = count($consumptionRequest);
+      $newConsumptionRequest = count($consumptionRequest);
       
       $stockRequest = $stockRequestRepository->findBy(['approvalStatus' => 1]);
-      $totalStock = count($stockRequest);
+      $newStockRequest = count($stockRequest);
+
+      $stock = $stockRepository->findBy(['approvalStatus' => 1]);
+      $newStock = count($stock);
+
+      $consumptionDelivery = $consumptionDeliveryRepository->findBy(['approvalStatus' => 1]);
+      $newConsumptionDelivery = count($consumptionDelivery);
 
     //   $sells = $sellsRepository->findBy(['approvalStatus' => 1]);
-      $totalGoods = count($stockRequest);
+      $totalNotifications = $newConsumptionRequest + $newStockRequest + $newStock + $newConsumptionDelivery;
 
     //   $students = $this->getDoctrine() 
     //                    ->getRepository('AppBundle:Department') 
     //                    ->findAll();
 
-        if ( $request->request->get('some_var_name')) {  
+
+  
+        if ( $request->isXmlHttpRequest()) {  
             $jsonData = array(
-                'consumption' => $totalConsumption,
-                'stock' =>  $totalStock,
-                'goods' => $totalGoods
+                'consumptionRequest' => $newConsumptionRequest,
+                'stockRequest' =>  $newStockRequest,
+                'stock' => $newStock,
+                'consumptionDelivery' => $newConsumptionDelivery,
+                'total' => $totalNotifications
             );  
-            
             return new JsonResponse($jsonData); 
         }
-        // if (!$request->isXmlHttpRequest()) {
+        // if (!$request->isXmlHttpRequest()) { 
         //     return new JsonResponse(array(
         //         'status' => 'Error',
         //         'message' => 'Error'),
         //     400);
         // }
-    
-        if($request->request->get('some_var_name')){
-            //make something curious, get some unbelieveable data
-            $arrData = ['output' => 'here the result which will appear in div'];
-            return new JsonResponse($arrData);
-        }
+        // if($request->request->get('some_var_name')){
+        //     //make something curious, get some unbelieveable data
+        //     $arrData = ['output' => 'here the result which will appear in div'];
+        //     return new JsonResponse($arrData);
+        // }
+       
+      
 
         return $this->render('notification/index.html.twig', [
             'controller_name' => 'NotificationController',
-            'consum'=>$consumptionRequest,
-            'totalconsumption' => $totalConsumption,
-            'totalstock' => $totalStock,
-            'totalgood' => $totalGoods
+                'consumptionRequest' => $newConsumptionRequest,
+                'stockRequest' =>  $newStockRequest,
+                'stock' => $newStock,
+                'consumptionDelivery' => $newConsuptionDelivery
         ]);
     }
 } 
